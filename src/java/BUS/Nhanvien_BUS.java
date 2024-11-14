e85e7e405f5dd3b96106908d0ed4381057e2f39c
hoangvu2k <161583344+hoangvu2k@users.noreply.github.com>
2024-11-14 21:43:01 +0700

    Update Nhanvien_BUS.java

--- src/java/BUS/Nhanvien_BUS.java
+++ src/java/BUS/Nhanvien_BUS.java
@@ -8,20 +8,29 @@ public class Nhanvien_BUS {
     private ArrayList<Nhanvien_DTO> listNhanvien;
     private Nhanvien_DAO nhanvien_DAO = new Nhanvien_DAO();
 
+    // Lấy danh sách nhân viên
     public ArrayList<Nhanvien_DTO> getList() {
         listNhanvien = nhanvien_DAO.getListNhanVien();
         return listNhanvien;
     }
 
+    // Thêm nhân viên
     public boolean themNhanVien(Nhanvien_DTO nhanVien) {
         return nhanvien_DAO.addNhanVien(nhanVien);
     }
 
+    // Sửa nhân viên
     public boolean suaNhanVien(Nhanvien_DTO nhanVien) {
         return nhanvien_DAO.updateNhanVien(nhanVien);
     }
 
-    public boolean xoaNhanVien(int maNV) { // Thay đổi kiểu tham số
-        return nhanvien_DAO.deleteNhanVien(maNV); // Truyền mã nhân viên
+    // Xóa nhân viên
+    public boolean xoaNhanVien(int maNV) {
+        return nhanvien_DAO.deleteNhanVien(maNV);
+    }
+
+    // Tìm kiếm nhân viên theo mã hoặc tên
+    public ArrayList<Nhanvien_DTO> timKiemNhanVien(String keyword) {
+        return nhanvien_DAO.searchNhanVien(keyword);
     }
 }
